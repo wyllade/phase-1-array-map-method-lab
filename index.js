528524ed0d017000fd3d2cf3c535f2dc156e89ee
Emman Oduor <wyllademanu003@gmail.com>
2025-03-12 11:33:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+ function titleCased () {
+  return tutorials.map(tutorials => {
+    return tutorials.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    
+  } );
+
 }
+
+console.log(titleCased())
